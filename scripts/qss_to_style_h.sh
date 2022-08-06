8ea4a4f30906e0164d26cea4885ef145584d67e5
cqroot <cqroot@outlook.com>
2022-08-07 01:18:34 +0800

    refactor: remove useless comments

--- scripts/qss_to_style_h.sh
+++ scripts/qss_to_style_h.sh
@@ -2,28 +2,6 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-# {
-#     echo '#ifndef LITE_MSGBOX_STYLE_H'
-#     echo '#define LITE_MSGBOX_STYLE_H'
-#     echo ''
-#     echo '#include <QString>'
-#     echo ''
-#     echo 'QString DefaultStyle() {'
-#     echo -n '    return'
-
-#     IFS=''
-#     while read -r line; do
-#         echo
-#         echo -n "        \"$line\""
-#     done <"${SCRIPT_DIR}/../qss/default.qss"
-
-#     echo ';'
-#     echo '}'
-#     echo
-#     echo '#endif //LITE_MSGBOX_STYLE_H'
-
-# } >"${SCRIPT_DIR}/../src/style.h"
-
 {
     echo '#ifndef LITE_MSGBOX_STYLE_H'
     echo '#define LITE_MSGBOX_STYLE_H'
